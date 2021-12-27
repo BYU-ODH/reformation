3714ddabbb1fbcf9cbac1807d5064fdfab4f96a1
Tory Anderson <torys.anderson@gmail.com>
2021-12-27 15:09:19 -0700

    v16

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject byu-odh/reformation "15"
+(defproject byu-odh/reformation "16"
   :description "Generate and manipulate form datastructures in pure Clojure, which can be read with Reagent."
   :url "https://github.com/BYU-ODH/reformation"
   :license {:name "Eclipse Public License"
