624bca39eafb153f755361ae51c36ce31f0b0861
Tory Anderson <torys.anderson@gmail.com>
2022-12-19 23:34:44 -0700

    div for text inputs too

--- test/cljs/reformation/review_front.cljs
+++ test/cljs/reformation/review_front.cljs
@@ -32,7 +32,7 @@
                :timetable "timetable"})
 
  (def test-form-vals ; 2022305
-  {:myhidden-text "whisper", :mymultitable "", :mycheckbox false, :mymulti [{:item "uea", :amount "2", :purpose "uea"} {:item "aoeu", :amount "-1", :purpose "aoeu"}], :mytextarea "some area stuff", :myselect "", :mydefault-text "something good", :amount-total 1, :mytoggle true, :myradio "", :myfileupload "", :mytext "stuff", :test "togged"})
+  {:myhidden-text "whisper", :mymultitable "", :mycheckbox false, :mymulti [{:item "uea", :amount "2", :purpose "uea"} {:item "aoeu", :amount "-1", :purpose "aoeu"}], :mytextarea "some area stuff", :myselect "", :mydefault-text "something good but really, really, really long", :amount-total 1, :mytoggle true, :myradio "", :myfileupload "", :mytext "stuff", :test "togged"})
 
 (defn app-page []
   ;[:h3 [:b "Review page"]]
