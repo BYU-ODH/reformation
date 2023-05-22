8dcedeaee9a8a241af54b022242f246ee3311bbd
Tory Anderson <torys.anderson@gmail.com>
2023-05-22 13:48:50 -0600

    refactored as reitit data structures

--- test/clj/reformation/routes/home.clj
+++ test/clj/reformation/routes/home.clj
@@ -1,16 +1,13 @@
 (ns reformation.routes.home
-  (:require [reformation.layout :as layout]
-            [compojure.core :refer [defroutes context GET POST ]]
-            [ring.util.http-response :as response]
-            [ring.util.io :refer [piped-input-stream]]
-            [ring.util.response :as r]
-            [clojure.java.io :as io]))
+  (:require [reformation.layout :as layout]))
 
 (defn home-page [req]
   (let [username (:username req)
         userinfo {:username "dummy"}]
     (layout/hiccup-render-cljs-base userinfo)))
 
-(defroutes home-routes
-  (GET "/" req (home-page req))
-  (GET "/review" req (home-page req)))
+(defn home-routes
+  "All the basic routes to be handled by the SPA"
+  []
+  ["/" {:get home-page}
+   "/review" {:get home-page}])
