cbf9d419154c671efc934ee5c1ffa7037f9cc8dc
Tory Anderson <torys.anderson@gmail.com>
2023-05-22 14:25:58 -0600

    matched appropiately the handlers

--- test/clj/reformation/handler.clj
+++ test/clj/reformation/handler.clj
@@ -1,25 +1,40 @@
 (ns reformation.handler
+  "Handlers for the testing setup"
   (:require [compojure.core :refer [routes wrap-routes]]
             [reformation.layout :refer [error-page]]
             [reformation.routes.home :refer [home-routes]]
-            [compojure.route :as route]
-            [ring.util.http-response :as response]
-            [ring.middleware.format-params :as mformat]
             [reformation.env :refer [defaults]]
             [mount.core :as mount]
-            [reformation.middleware :as middleware]))
+            [reformation.middleware :as middleware]
+            [reitit.ring :as ring]
+            [ring.middleware.content-type :refer [wrap-content-type]]
+            [ring.middleware.webjars :refer [wrap-webjars]]            
+            [clojure.tools.logging :as log]))
 
 (mount/defstate init-app
   :start ((or (:init defaults) identity))
   :stop  ((or (:stop defaults) identity)))
 
-(defn app-routes []
-  (routes
-   (-> #'home-routes
-       (wrap-routes middleware/wrap-formats))
-   (route/not-found
-    (:body
-     (error-page {:status 404
-                  :title "page not found"})))))
-
-(defn app [] (middleware/wrap-base (app-routes)))
+(mount/defstate app
+  :start
+  (do
+    (log/info "Starting ring-handler")
+    (ring/ring-handler
+     (ring/router
+      [(home-routes)])
+     (ring/routes
+      (ring/create-resource-handler
+       {:path "/"})
+      (wrap-content-type
+       (wrap-webjars (constantly nil)))
+      (ring/create-default-handler
+       {:not-found #(do (println "Not found:" (prn-str %))
+                        (error-page {:status 404
+                                     :title "404 - Page not found."
+                                     :message "The page you are looking for is not found. Please check URL again."}))
+        :method-not-allowed #(do
+                               (constantly (error-page {:status 405
+                                                        :title "405 - Not allowed"})))
+        :not-acceptable #(do
+                           (constantly (error-page {:status 406
+                                                    :title "406 - Not acceptable"})))})))))
