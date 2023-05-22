58a7c3bae75adef1659bbbb27bfd8b2728309aa2
Tory Anderson <torys.anderson@gmail.com>
2023-05-22 16:34:40 -0600

    timbre logging, all online

--- project.clj
+++ project.clj
@@ -68,7 +68,8 @@
                                   [ring/ring-defaults "0.3.2"] ;; CLJ routing
                                   [secretary "1.2.3"] ;; CLJS routing
                                   [re-frame "0.11.0"]
-                                  [venantius/accountant "0.2.4"]]
+                                  [venantius/accountant "0.2.4"]
+                                  [com.taoensso/timbre "5.1.2"]]
                    :plugins [[lein-codox "0.10.7"]
                              [com.jakemccrary/lein-test-refresh "0.14.0"]
                              [lein-doo "0.1.7"]
